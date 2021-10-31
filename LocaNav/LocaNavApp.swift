23ec0098bc79a440cca47a69c80973c006a7c8a6
dusky1ight <33296087+dusky1ight@users.noreply.github.com>
2021-10-31 15:42:59 +0530

    Initial Commit

---
+++ LocaNav/LocaNavApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocaNavApp.swift
+//  LocaNav
+//
+//  Created by Prateek on 31/10/21.
+//
+
+import SwiftUI
+
+@main
+struct LocaNavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
